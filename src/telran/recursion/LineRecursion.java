1739d1b977578bf92fe6f9d17c93d1afa89947c8
StasSaltykov <stasyanych92@gmail.com>
2022-08-18 19:03:12 +0300

    hw#18 fix

--- src/telran/recursion/LineRecursion.java
+++ src/telran/recursion/LineRecursion.java
@@ -1,36 +1,37 @@
 package telran.recursion;
 
 public class LineRecursion {
-	
 	public static long factorial(int n) {
-		
 		if (n == 0) {
 			return 1;
 		}
 		return n * factorial(n - 1);
 	}
-	
+
 	/**
 	 * 
 	 * @param a either negative or positive
 	 * @param b positive
 	 * @return a ^ b
 	 */
-	public static long pow(int a, int b) {
-	    if(b == 0) {
-	        return 1;
-	    }
-		return multiplication(a, pow(a, b - 1));  
-	}
-	//FIXME
-	private static long multiplication(int a, long pow) {
-		if (pow == 0 || a == 0) {
-			return 0;
-		} 
-		if (a < 0) {
-			return -a + multiplication(-a, pow - 1);
+	static public long pow(int a, int b) {
+		if (b < 0) {
+			throw new IllegalArgumentException(" power can't be a negative");
 		}
-		return a + multiplication(a, pow - 1);
+		if (b == 0) {
+			return 1;
+		}
+		return multiply(a, pow(a, b - 1));
+	}
+
+	private static long multiply(int x, long y) {
+		if (y < 0) {
+			return multiply(-x, -y);
+		}
+		if (y == 0) {
+			return 0;
+		}
+		return x + multiply(x, y - 1);
 	}
 
 	/**
@@ -38,25 +39,28 @@ public class LineRecursion {
 	 * @param x
 	 * @return x ^ 2
 	 */
-	
 	public static int square(int x) {
+		// no cycles
+		// no * / allowed
+		// no call any additional function
+		// no static fields
 		if (x < 0) {
-			x = -x;
+			return square(-x);
 		}
-	    if(x == 0){
-	        return 0;
-	    } else {
-	    	return square(x - 1) + x + x - 1;
-	    }  
+		if (x == 1) {
+			return 1;
+		}
+		// (x - 1)^2 = x^2 - 2x + 1 => x^2 = (x - 1)^2 + 2x -1
+		return x + x - 1 + square(x - 1);
 	}
-	
+
 	/**
 	 * 
-	 * @param ar - array of integer numbers
+	 * @param ar - array of in teger numbers
 	 * @return sum of all numbers from the given array
 	 */
 	public static int sum(int ar[]) {
-
+		// no cycles
 		return sum(0, ar);
 	}
 
@@ -67,8 +71,3 @@ public class LineRecursion {
 		return ar[firstIndex] + sum(firstIndex + 1, ar);
 	}
 }
-
-
-
-
-
--- src/telran/recursion/tests/RecursionTest.java
+++ src/telran/recursion/tests/RecursionTest.java
@@ -8,41 +8,44 @@ import org.junit.jupiter.api.Test;
 import static telran.recursion.LineRecursion.*;
 
 class RecursionTest {
+	int count = 0;
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
-	
+
 	@Test
 	void factorialTest() {
 		assertEquals(24, factorial(4));
 	}
-	
+
 	@Test
 	void powTest() {
 		assertEquals(1000, pow(10, 3));
 		assertEquals(-1000, pow(-10, 3));
 		assertEquals(100, pow(-10, 2));
-		assertEquals(1, pow(10, 0));
-		assertEquals(10, pow(10, 1));
 	}
-	
+
 	@Test
 	void sumTest() {
-		int ar[] = {1, 2, 3, 4};
+		int ar[] = { 1, 2, 3, 4 };
 		assertEquals(10, sum(ar));
 	}
-	
+
 	@Test
 	void squareTest() {
-		assertEquals(4, square(2));
-		assertEquals(9, square(-3));
+		assertEquals(100, square(10));
+		assertEquals(100, square(-10));
 	}
-	
+
+//	private void f() {
+//		
+//		if(Math.random() < 0.99) {
+//			f();
+//			count++;
+//		}
+//		
+//		
+//	}
+
 }
-
-
-
-
-
-
